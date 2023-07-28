50962ee23ebe29159d07e32f23a9c7fba9565f30
Carlos Bustamante <contact@charlyfive.com>
2023-07-28 17:50:05 -0600

    Servidor actualiozado y función de request

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from keras.applications import MobileNet
 from keras.applications.mobilenet import preprocess_input, decode_predictions
 from keras.utils import img_to_array
@@ -8,19 +7,50 @@ import numpy as np
 import io
 
 model = MobileNet(weights="imagenet")
-dummy = model.predict(np.random.rand(1, 224, 224, 3), verbose=0)
+
+# Warm-up
+dummy_input = np.random.rand(1, 224, 224, 3)
+dummy_output = model.predict(dummy_input)
+
 app = FastAPI()
 
 
-@app.post("/predict/")
-async def predict_class(file: UploadFile = File(...)):
-    image_bytes = await file.read()
+def preprocess_image(image_bytes: bytes):
+    """
+    Preprocesamiento de la imagen.
+
+    Args:
+        image_bytes
+
+    Return:
+        processed image
+    """
     img = Image.open(io.BytesIO(image_bytes))
     img = img.resize((224, 224))
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     pImg = preprocess_input(img_array)
+    return pImg
 
-    prediction = model.predict(pImg, verbose=0)
+
+def predict(pImg: None):
+    """
+    Procesa la imagen y realiza la predicción:
+
+    Args:
+        pImg
+
+    Return:
+        Diccionario
+    """
+    prediction = model.predict(pImg)
     _, predicted_class, probability = decode_predictions(prediction, top=1)[0][0]
     return {"class": str(predicted_class), "probability": str(probability)}
+
+
+@app.post("/predict/")
+async def predict_class(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    pImg = preprocess_image(image_bytes)
+    result = predict(pImg)
+    return result
