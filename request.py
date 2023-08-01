2091bfa40259844ee251e73bfb50a1b6648db97a
Carlos Alberto Bustamante Gaytan <charlyfive07@gmail.com>
2023-08-01 16:27:42 -0600

    Request modificado y labels añadidos

--- request.py
+++ request.py
@@ -1,8 +1,24 @@
 import requests
 from time import time
+import numpy as np
+import ast
+
+# Ruta del archivo
+ruta_archivo = "labels.txt"
+
+
+# Función para leer el archivo y obtener su contenido como cadena de texto
+def leer_archivo(ruta):
+    with open(ruta, "r") as archivo:
+        contenido = archivo.read()
+    return contenido
+
+
+contenido_txt = leer_archivo(ruta_archivo)
+labels = ast.literal_eval(contenido_txt)
 
 url = "http://192.168.3.116:8000/predict/"
-file_path = "images/test3.jpg"
+file_path = "images/test1.jpg"
 
 start = time()
 with open(file_path, "rb") as file:
@@ -12,7 +28,8 @@ with open(file_path, "rb") as file:
     response = response.json()
     end = time()
     print(
-        f"Clase: {response['class']} con probabilidad de {float(response['probability'])*100:.4f}%"
+        f"Clase: {labels[int(response['class'])]} con probabilidad de {float(response['probability'])*100:.4f}%"
     )
+    print(f"Tiempo de predicción: {response['time']:.4f}s")
     print(f"Tiempo de request: {(end_request-start_request):.4f}s")
     print(f"Tiempo total {(end-start):.4f}")
