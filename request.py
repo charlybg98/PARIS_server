95500e460fadbd4f62d5088f2f3e9312f60c7e61
Carlos Alberto Bustamante Gaytan <charlyfive07@gmail.com>
2023-08-01 16:29:37 -0600

    Request modificado y labels añadidos

--- request.py
+++ request.py
@@ -3,25 +3,23 @@ from time import time
 import numpy as np
 import ast
 
-# Ruta del archivo
-ruta_archivo = "labels.txt"
 
-
-# Función para leer el archivo y obtener su contenido como cadena de texto
 def leer_archivo(ruta):
     with open(ruta, "r") as archivo:
         contenido = archivo.read()
     return contenido
 
 
+ruta_archivo = "labels.txt"
+url = "http://192.168.3.116:8000/predict/"
+image_path = "images/test2.jpg"
+
+
 contenido_txt = leer_archivo(ruta_archivo)
 labels = ast.literal_eval(contenido_txt)
 
-url = "http://192.168.3.116:8000/predict/"
-file_path = "images/test1.jpg"
-
 start = time()
-with open(file_path, "rb") as file:
+with open(image_path, "rb") as file:
     start_request = time()
     response = requests.post(url, files={"file": file})
     end_request = time()
