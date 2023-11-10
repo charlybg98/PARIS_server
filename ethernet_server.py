c4f29aedd9560a164d9b9c7596bf5317fd6188df
Carlos Alberto Bustamante Gaytán <95149214+charlybg98@users.noreply.github.com>
2023-11-09 22:03:34 -0600

    Update ethernet_server.py

--- ethernet_server.py
+++ ethernet_server.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from PIL import Image
 import io
 import json
+import time
 
 # Carga el mapeo de índices de clase a etiquetas
 class_index_path = tf.keras.utils.get_file(
@@ -25,6 +26,14 @@ def preprocess_image(img):
     pImg = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
     return pImg
 
+# Realiza una inferencia de calentamiento con un tensor de ceros
+def warmup_inference():
+    # Crea un tensor de entrada de ceros
+    input_data = tf.zeros([1, 224, 224, 3], dtype=tf.float32)
+    func(input_tensor=input_data)
+
+warmup_inference()  # Realiza la inferencia de calentamiento
+
 # Función para realizar la inferencia
 def make_inference(image_data):
     # Convierte los datos de imagen en un objeto Image
@@ -36,16 +45,19 @@ def make_inference(image_data):
     preprocessed_image = preprocess_image(image_array)
     
     # Realiza la inferencia
+    start_time = time.time()
     preds = func(preprocessed_image)['predictions']
-    
+    inference_time = time.time() - start_time
+    print(f"Inferencia realizada en {inference_time:.2f} segundos")
+
     # Obtén la clase con la probabilidad más alta
     predicted_class = tf.argmax(preds[0]).numpy()
     confidence = preds[0][predicted_class].numpy()
-    
+
     # Obtén la etiqueta de la clase
     label = class_labels[predicted_class][1]  # Usando el mapeo cargado previamente
     
-    return label, confidence
+    return label, confidence, inference_time
 
 # Crea un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -85,11 +97,11 @@ while True:
                 print("Imagen recibida correctamente.")
 
                 # Realiza la inferencia
-                label, confidence = make_inference(image_data)
+                label, confidence, inference_time = make_inference(image_data)
                 print(f"Resultado de la inferencia: {label} con un {confidence*100:.2f}% de probabilidad")
 
                 # Envía el resultado de vuelta al cliente
-                result_message = f"La predicción es {label} con un {confidence*100:.2f}% de probabilidad."
+                result_message = f"La predicción es {label} con un {confidence*100:.2f}% de probabilidad. Tiempo de inferencia: {inference_time:.2f} segundos."
                 connection.sendall(result_message.encode('utf-8'))
 
             else:
